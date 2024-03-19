e15ef65508fed76fb07db401d161444145bc35e2
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-19 17:03:09 +0800

    java

--- HSMySQL2/src/main/java/com/hs/rest/gps/repository/GPSRepositoryImpl.java
+++ HSMySQL2/src/main/java/com/hs/rest/gps/repository/GPSRepositoryImpl.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.hs.rest.gps.model.GPS;
 
+@Repository
 public class GPSRepositoryImpl implements GPSRepository {
 	
 	@Autowired
