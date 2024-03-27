3a30c573dad0981a898ce390fe5fe9d94db03757
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-27 14:05:12 +0800

    java

--- HSMySQL2/src/main/java/com/hs/rest/gps/controller/GPSController.java
+++ HSMySQL2/src/main/java/com/hs/rest/gps/controller/GPSController.java
@@ -1,13 +1,8 @@
 package com.hs.rest.gps.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
