6fa05070391b28aa44097a9fd593227511773810
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-03 16:51:47 +0800

    java

--- HSMySQL2/src/main/webapp/rest/js/rest.js
+++ HSMySQL2/src/main/webapp/rest/js/rest.js
@@ -4,11 +4,17 @@ const $ = (id) => document.getElementById(id);
 
 // 通用函數，用於檢查事件目標是否包含指定的類別，如果是則執行回調函數
 const handleEvent = async (event, className, callback) => {
+	var gpsId;
     if (event.target.classList.contains(className)) {
 		switch(className) {
 			case 'gps-add': // 新增GPS
 				await callback();
 				break;
+			case 'update-gps-button': // 修改客戶
+			case 'delete-gps-button': // 刪除客戶
+				gpsId = event.target.getAttribute('data-id');
+				await callback(gpsId);
+				break;	
 		}
     }
 };
@@ -74,6 +80,81 @@ const handleAddGPS = async () => {
 	}
 };
 
+// 處理修改GPS的異步邏輯
+const handleUpdateGPS = async (gpsId) => {
+	const getGPSUrl = `${REMOTE_URL}../mvc/gps/${gpsId}`;
+	const gpsResponse = await fetch(getGPSUrl);
+	const { status, message, data } = await gpsResponse.json();
+	console.log(status, message, data);
+	
+	// 使用 Swal.fire 白底彈窗請求修改用戶
+	const result = await Swal.fire({
+		title: '修改GPS',
+		html: `
+            <input id="gps-latitude-input" class="swal2-input" value="${data.latitude}" placeholder="請輸入緯度">
+	        <input id="gps-longitude-input" class="swal2-input" value="${data.longitude}" placeholder="請輸入經度">
+	        <input id="gps-location-input" class="swal2-input" value="${data.location}" placeholder="請輸入地址">
+	        <input id="gps-locationName-input" class="swal2-input" value="${data.locationName}" placeholder="請輸入單位名稱">
+	        <input id="gps-meter-input" type="number" class="swal2-input" value="${data.meter}" placeholder="請輸入公尺">`,
+		focusConfirm: false,
+		preConfirm: () => {
+	        const latitude = $('gps-latitude-input').value;
+	        const longitude = $('gps-longitude-input').value;
+	        const location = $('gps-location-input').value;
+	        const locationName = $('gps-locationName-input').value;
+	        const meter = $('gps-meter-input').value;
+	        
+	        // 檢查輸入值，如果需要的話
+	        if (!latitude || !longitude || !location || !locationName || !meter) {
+	            Swal.showValidationMessage("所有欄位都是必填的");
+	            return false; // 阻止彈窗關閉
+	        }
+	
+	        return { latitude, longitude, location, locationName, meter };
+	    }
+	});
+	
+	if (result.value) {
+		console.log(result.value);
+		console.log('Role:', result.value.role);
+		console.log('Username:', result.value.username);
+	} else {
+		console.log('No values');
+		return;
+	}
+	
+	// 進行遠端修改
+	const fullUrl = `${REMOTE_URL}../mvc/gps/${gpsId}`;
+	try {
+		const response = await fetch(fullUrl, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				latitude: result.value.latitude,
+				longitude: result.value.longitude,
+				location: result.value.location,
+				locationName: result.value.locationName,
+				meter: result.value.meter
+			})
+		});
+		if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+		const { status, message } = await response.json();
+		if (!status) throw new Error(`Failed to add customer: ${message}`);
+		// 重新渲染GPS列表
+		fetchAndRenderData('../mvc/gps', 'gps-body', renderGPS);
+	} catch (error) {
+		console.error('Error adding customer:', error);
+	}
+	
+};
+
+// 處理刪除客戶的異步邏輯
+const handleDeleteGPS = async (gpsId) => {
+	handleDelete(`${REMOTE_URL}../mvc/gps/${gpsId}`, () => fetchAndRenderData('../mvc/gps', 'gps-body', renderGPS));
+};
+
 // 渲染GPS列表的函數
 const renderGPS = ({ id, latitude, longitude, location, locationName, meter }) => `
     <tr>
@@ -83,7 +164,48 @@ const renderGPS = ({ id, latitude, longitude, location, locationName, meter }) =
         <td>${location}</td>
         <td>${locationName}</td>
         <td>${meter}</td>
-    </tr>`;
+        <td title='修改'><span class="button-update pure-button update-gps-button" data-id="${id}">修改</span></td>
+        <td title='刪除'><span class="button-delete pure-button delete-gps-button" data-id="${id}">刪除</span></td>
+	</tr>`;
+
+// 通用刪除
+// 使用 Swal.fire 跳出 yes/no 彈窗來決定是否要刪除
+const handleDelete = async (fullUrl, fetchAndRender) => {
+	const result = await Swal.fire({
+		title: '確定要刪除嗎?',
+		text: "刪除後將無法恢復!",
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonText: '是的，刪除它!',
+		cancelButtonText: '取消'
+	});
+	if (result.isConfirmed) {
+		console.log('Confirmed');
+	} else {
+		console.log('Cancelled');
+		return;
+	}
+
+	// 進行遠端刪除
+	// delete fullUrl
+	try {
+		const response = await fetch(fullUrl, { method: 'DELETE' });
+		if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+		const { status, message } = await response.json();
+		if (!status) throw new Error(`Failed to delete: ${message}`);
+		// 重新渲染列表
+		fetchAndRender();
+	} catch (error) {
+		console.error('Delete error', fullUrl, error);
+		// Swal.fire dialog
+		Swal.fire({
+			title: '刪除失敗!',
+			text: error,
+			icon: 'error',
+			confirmButtonText: '確定'
+		});
+	}
+};
 
 // 使用解構賦值和模板字符串來簡化代碼
 const fetchAndRenderData = async (url, containerId, renderFn) => {
@@ -134,5 +256,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 		event.preventDefault();  // 取消默認動作，這裡是阻止超鏈接跳轉
 		console.log(event.target);
 		await handleEvent(event, 'gps-add', handleAddGPS);
+	});
+	
+	$("gps-body").addEventListener("click", async (event) => {
+		event.preventDefault();  // 取消默認動作，這裡是阻止超鏈接跳轉
+		console.log(event.target);
+		await handleEvent(event, 'update-gps-button', handleUpdateGPS);
+		await handleEvent(event, 'delete-gps-button', handleDeleteGPS);
 	});
 });
