5391fe47cd9fecd1fd5d5ee137a4fc14e07622c6
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-05 15:00:05 +0800

    java

--- HSMySQL2/src/main/java/com/hs/rest/key/repository/ArgKeyRepository.java
+++ HSMySQL2/src/main/java/com/hs/rest/key/repository/ArgKeyRepository.java
@@ -3,7 +3,10 @@ package com.hs.rest.key.repository;
 import com.hs.rest.key.model.po.ArgKey;
 
 public interface ArgKeyRepository {
+	// 比對 key 是否正確
 	public Boolean checkKey(String key);
-	public ArgKey findById(Integer id);
+	// 取得資料 id 固定是 7
+	public ArgKey getArgKey();
+	// 更新資料
 	public void update(ArgKey argKey);
 }
