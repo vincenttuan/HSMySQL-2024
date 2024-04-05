fb3efc4852f82b3369b89bedcbc3a41e8e06a205
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-05 14:55:23 +0800

    java

--- HSMySQL2/src/main/java/com/hs/rest/key/model/po/ArgKey.java
+++ HSMySQL2/src/main/java/com/hs/rest/key/model/po/ArgKey.java
@@ -7,7 +7,7 @@ import lombok.Data;
 // id 一率都是 7
 // id, name, strArg1, memo
 @Data
-public class Args {
+public class ArgKey {
     private final Integer id = 7; // 固定值(表示:登入首頁權限密鑰)
     private String name;
     private String strArg1; // 放 key 內容
